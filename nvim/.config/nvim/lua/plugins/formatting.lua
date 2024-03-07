1a157a4b037c4a120ecb5e74aaa8f9f07c2677ee
Willians Faria <dev.willians.faria@gmail.com>
2024-03-06 21:09:50 -0300

    removing webdevicons

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -3,26 +3,9 @@ return {
     event = { "BufWritePre", "BufNewFile" },
     config = function()
         local conform = require("conform")
-
         conform.setup({
-            formatters_by_ft = {
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                html = { "prettier" },
-                json = { "prettier" },
-                jsonc = { "prettier" },
-                yaml = { "prettier" },
-                markdown = { "markdownlint" },
-                css = { "prettier" },
-                scss = { "prettier" },
-                lua = { "stylua" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                timeout_ms = 500,
-            },
+            formatters_by_ft = { lua = { "stylua" } },
+            format_on_save = { lsp_fallback = true, timeout_ms = 500 }
         })
     end,
 }
--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,5 @@
 return {
     'stevearc/oil.nvim',
-    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("oil").setup({})
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
