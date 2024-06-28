2df444d0338afeda5b1d08c2938e981d402c0e00
Willians Faria <dev.willians.faria@gmail.com>
2024-06-27 22:09:33 -0300

    i still use conform on macl ol

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,3 +1,16 @@
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, { desc = 'Disable autoformat-on-save' })
+
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, { desc = 'Re-enable autoformat-on-save' })
+
 return {
   'stevearc/conform.nvim',
   event = 'BufWritePre',
@@ -11,11 +24,10 @@ return {
         javascript = { 'prettier' },
         javascriptreact = { 'prettier' },
       },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+        return { timeout_ms = 500, lsp_format = 'fallback' }
+      end,
     }
   end,
 }
-
