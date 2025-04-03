a2ccdd928e0e7c464616dfc86381134dbb3da8e4
wiru <dev.willians.faria@gmail.com>
2025-04-03 15:54:00 -0300

    color my stuffs

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -21,78 +21,21 @@ end
 
 local colorschemes = {
   default = { "default", noreturn = true },
-  gruber_darker = {
-    "thimc/gruber-darker.nvim",
-    config = function() set_colorscheme("gruber-darker") end,
-  },
   kanagawa = {
     "rebelot/kanagawa.nvim",
     priority = 1000,
     config = function()
       require("kanagawa").setup({
-        -- colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
+        colors = { theme = { all = { ui = { bg_gutter = "none" } } } },
         background = { dark = "dragon", light = "lotus" },
         transparent = false,
       })
       set_colorscheme("kanagawa")
     end,
   },
-  tokyodark = {
-    "tiagovla/tokyodark.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function() set_colorscheme("tokyodark") end,
-  },
-  tokyonight = {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function() set_colorscheme("tokyonight-day") end,
-  },
-  vague = {
-    "vague2k/vague.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("vague").setup({})
-      set_colorscheme("vague")
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#121314", fg = "#cdcdcd" })
-    end,
-  },
-  lackluster = {
-    "slugbyte/lackluster.nvim",
-    lazy = false,
-    priority = 1000,
-    init = function() set_colorscheme("lackluster-hack") end,
-  },
-  github = {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("github-theme").setup({})
-      set_colorscheme("github_light")
-    end,
-  },
-  gruvbox = {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup({})
-      set_colorscheme("gruvbox")
-    end,
-  },
-  autumn_night = {
-    dir = "~/code/autumn_night.nvim",
-    priority = 1000,
-    dev = true,
-    config = function() set_colorscheme("autumn_night") end,
-  },
 }
 
-local current = "autumn_night"
+local current = "kanagawa"
 local scheme = colorschemes[current]
 if scheme.noreturn then
   set_colorscheme()
